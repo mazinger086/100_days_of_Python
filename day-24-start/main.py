111b8f062e5c3cba1902f91040e09d00437b775a
Daniel Lasauskas <Mazinger_086@yahoo.com>
2023-07-18 10:26:21 -0300

    mail merge finished

--- day-24-start/main.py
+++ day-24-start/main.py
@@ -1,10 +1,11 @@
 # De esta manera con WITH NO tenemos que usar el file.close()
-# with open("my_file.txt") as file:
-#     contents = file.read()
-#     print(contents)
+# Absolute path: /Users/v-dlasauskas/Desktop/my_file.txt
+# Relative path: ../../my_file.txt
+# OJO con las barras invertidas ya que se toman como caracter de escape usa el r"" (raw string)
+with open("../../my_file.txt") as file:
+    contents = file.read()
+    print(contents)
 
 # Mode: r=read, a=append, w=write
-with open("my_file2.txt", mode="w") as file:
-    file.write("\nNew text...")
-
-
+# with open("my_file2.txt", mode="w") as file:
+#     file.write("\nNew text...")
